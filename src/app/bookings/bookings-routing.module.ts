d98dc05bfbdbecf9b9fe4b4a1d9e531132181c50
Rodrigo Freund de Moraes <rodrigofreund@gmail.com>
2024-02-02 23:39:30 +0000

    first implementation

---
+++ src/app/bookings/bookings-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { BookingsPage } from './bookings.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BookingsPage
+  },
+  {
+    path: 'offer-bookings',
+    loadChildren: () => import('./offer-bookings/offer-bookings.module').then( m => m.OfferBookingsPageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class BookingsPageRoutingModule {}
---
+++ src/app/places/places.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Place } from './discover/place.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+
+  private _places: Place[] = [
+    new Place(
+      'p1',
+      'Manhattan Mansion',
+      'In the heart of New York City.',
+      'https://upload.wikimedia.org/wikipedia/commons/6/67/Manhattan_skyline_south_by_Don_Ramey_Logan.jpg',
+      149.99),
+    new Place(
+        'p2',
+        'São Paulo',
+        'The San Jose Cathedral.',
+        'https://media.lex.dk/media/180126/article_topimage_20230920-030206-6-4528x3016ma_top.jpg',
+        40.00),
+    new Place(
+        'p3',
+        'Porto',
+        'Ibis Hotel',
+        'https://www.feelporto.com/blog/wp-content/uploads/2021/07/28_visitar-Porto.jpg',
+        25.00)
+    
+  ];
+
+  constructor() { }
+
+  getPlaces() {
+    return [...this._places];
+  }
+}
